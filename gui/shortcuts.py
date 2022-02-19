f29ea8ba90a341b1db05a0e39c2c9f938275e730
ente <10390798+BrittaEnte@users.noreply.github.com>
2022-02-19 19:35:54 +0100

    19.02.2022

---
+++ gui/shortcuts.py
@@ -0,0 +1,19 @@
+# str plus mausklick markiert die zeilen
+# und dann kann man alle ändern 
+
+
+ a = diesistneuundgeil(text = 'ente')
+
+B = diesistneuundgeil(text = 'ente')
+
+#str plus D select the first item and then the next one with the same name. cool 
+
+c = diesistneuundgeil(text = 'ente')
+
+d =diesistneuundgeil(text = 'ente')
+
+#str plus K wird die momentane idee überspringen und das nächste kopiern
+
+#str plus p wird alle offenen dateien anzeigen und man kann dann wählen bzw man kann auch die ersten buchstaben eingeben
+
+#str plus alt + number macht weitere panele auf, geht nicht
